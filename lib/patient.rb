2211e9bd5d7e1a1f3758148f0aa9a5ea4bb97c96
Parker Williamson <parker.williamson@gmail.com>
2020-05-20 20:24:53 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,9 +1,15 @@
 class Patient
 
   attr_reader :name, :appointment, :doctor
+  
+  @@all = []
 
   def initialize(name)
     @name = name
+  end
+  
+  def self.all
+    @@all 
   end
 
   def new_appointment(date, doctor)
